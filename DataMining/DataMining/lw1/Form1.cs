ded313cb0f0aa3fa8760888e990ec31f39dcfff7
Yelyzaveta Shulenko <liza.shulenko@gmail.com>
2021-10-07 16:22:25 +0300

    LW1

---
+++ DataMining/DataMining/lw1/Form1.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace LW1
+{
+    public partial class Form1 : Form
+    {
+        private readonly string _filePath = @"F:\univer\sms-spam-corpus.csv";
+        private readonly string[] _stopWords = { "in", "to", "the", "a", "n", "and", "at" };
+
+        private readonly Regex _specialSymbolsRegex = new Regex(@"[^\w\s']|[\d_]|('\w+)", RegexOptions.Compiled);
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            Run();
+        }
+
+        private void Run()
+        {
+            var hamList = new List<string>();
+            var spamList = new List<string>();
+
+            LoadEditedString(_filePath, ref hamList, ref spamList);
+
+            var hamDictionary = CountWords(String.Join(" ", hamList)).OrderByDescending(x => x.Value).ToList();
+            var spamDictionary = CountWords(String.Join(" ", spamList)).OrderByDescending(x => x.Value).ToList();
+
+            WriteDictionaryToFile(hamDictionary, "F:/univer/test", "hamDictionary.txt");
+            WriteDictionaryToFile(spamDictionary, "F:/univer/test", "spamDictionary.txt");
+
+            Utillities.DrawNewChart(hamDictionary, chartHamWordLength);
+            Utillities.DrawNewChart(spamDictionary, chartSpamWordLength);
+
+            labelHamAverageLength.Text = String.Format("Ham words average length: {0:F2}", Utillities.CountAverageLength(hamDictionary));
+            labelSpamAverageLength.Text = String.Format("Spam words average length: {0:F2}", Utillities.CountAverageLength(spamDictionary));
+
+            Utillities.DrawNewChart(hamList, chartHamNoticeLength);
+            Utillities.DrawNewChart(spamList, chartSpamNoticeLength);
+
+            labelHamNoticeAvgLength.Text = String.Format("Ham notices average length: {0:F2}", Utillities.CountAverageLength(hamList));
+            labelSpamNoticeAvgLength.Text = String.Format("Spam notices average length: {0:F2}", Utillities.CountAverageLength(spamList));
+
+            Utillities.CountWordsLength(hamDictionary, chartHamFrequentWords);
+            Utillities.CountWordsLength(spamDictionary, chartSpamFrequentWords);
+        }
+
+        private Dictionary<string, int> CountWords(string text)
+        {
+            var dictionary = new Dictionary<string, int>();
+
+            var words = text.Split(' ');
+
+            foreach (var word in words)
+            {
+                if (String.IsNullOrEmpty(word))
+                {
+                    break;
+                }
+
+                if (dictionary.ContainsKey(word))
+                {
+                    dictionary[word]++;
+                }
+                else
+                {
+                    dictionary.Add(word, 1);
+                }
+            }
+
+            return dictionary;
+        }
+
+        private void LoadEditedString(string filePath, ref List<string> hamListResult, ref List<string> spamListResult)
+        {
+            using (StreamReader streamReader = new StreamReader(filePath))
+            {
+                while (!streamReader.EndOfStream)
+                {
+                    var line = streamReader.ReadLine().TrimEnd(',');
+
+                    var splitedLineArray = line.Split(new char[] { ',', '"' }, 2);
+
+                    var key = splitedLineArray[0].Trim();
+
+                    var editedSentenseSB = new StringBuilder();
+
+                    foreach (var valueRaw in splitedLineArray[1].Split(' '))
+                    {
+                        var value = GetClearedString(valueRaw);
+
+                        if (String.IsNullOrEmpty(value))
+                        {
+                            continue;
+                        }
+
+                        editedSentenseSB.Append($"{value} ");
+                    }
+
+                    if (key == "spam")
+                    {
+                        spamListResult.Add(editedSentenseSB.ToString().Trim());
+                    }
+                    else if (key == "ham")
+                    {
+                        hamListResult.Add(editedSentenseSB.ToString().Trim());
+                    }
+                }
+            }
+        }
+
+        private string GetClearedString(string rawString)
+        {
+            if (String.IsNullOrEmpty(rawString))
+            {
+                return null;
+            }
+
+            var result = _specialSymbolsRegex.Replace(rawString.Trim().ToLower(), "");
+
+            if (_stopWords.Any(stopWord => stopWord == result))
+            {
+                return null;
+            }
+
+            return result.Trim();
+        }
+
+        private void WriteDictionaryToFile(IEnumerable<KeyValuePair<string, int>> dictionary, string filePath, string fileName)
+        {
+            using (var streamWriter = new StreamWriter($"{filePath}/{fileName}"))
+            {
+                foreach (var keyValuePair in dictionary)
+                {
+                    streamWriter.WriteLine($"{keyValuePair.Key}: {keyValuePair.Value}");
+                }
+            }
+        }
+
+    }
+}
+
+
---
+++ DataMining/DataMining/lw1/Utillities.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace LW1
+{
+    public static class Utillities
+    {
+        public static void DrawNewChart(IEnumerable<KeyValuePair<string, int>> dictionary, Chart chart)
+        {
+            var count = 0;
+
+            foreach (var keyValuePair in dictionary)
+            {
+                chart.Series[0].Points.AddXY(count, keyValuePair.Key.Length);
+                count++;
+            }
+        }
+        public static void DrawNewChart(IEnumerable<string> notificationList, Chart chart)
+        {
+            var count = 0;
+
+            foreach (var notise in notificationList)
+            {
+                chart.Series[0].Points.AddXY(count, notise.Length);
+                count++;
+            }
+        }
+        public static double CountAverageLength(IEnumerable<KeyValuePair<string, int>> dictionary)
+        {
+            double wordLengthSum = dictionary.Select(w => w.Key.Length).Sum();
+
+            double wordCount = dictionary.Count();
+
+            return wordLengthSum / wordCount;
+        }
+        public static double CountAverageLength(IEnumerable<string> notificationList)
+        {
+            double noticeLengthSum = notificationList.Select(n => n.Length).Sum();
+
+            double noticeCount = notificationList.Count();
+
+            return noticeLengthSum / noticeCount;
+        }
+        public static void CountWordsLength(IEnumerable<KeyValuePair<string, int>> dictionary, Chart chart)
+        {
+            chart.Series[0].Points.Clear();
+
+            var amount = 20;
+
+            foreach(var keyValuePair in dictionary)
+            {
+                if (amount == 0) break;
+                chart.Series[0].Points.AddXY(keyValuePair.Key, keyValuePair.Value);
+                amount--;
+            }
+        }
+    }
+}
